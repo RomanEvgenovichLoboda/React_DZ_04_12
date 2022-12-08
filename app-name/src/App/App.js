c03f9bf285d6ca9762c5276283899ab133298ad1
loboda.rom@gmail.com <loboda.rom@gmail.com>
2022-12-08 23:15:34 +0200

    first commit

---
+++ app-name/src/App/App.js
@@ -0,0 +1,31 @@
+import logo from '../pict/z1.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App App-logo">
+      <div className='div__main'>
+        <div className='div__left pict'>
+          
+        </div>
+        <div className='div__right'>
+          <div className='text__grey'>Связатся</div>
+          <div className='text__bold'>loboda.rom@gmail.com</div>
+          <div className='text__bold'>+380987777777</div>
+          <div className='text__grey'>Адрес</div>
+          <div className='text__bold'>Украина,</div>
+          <div className='text__bold'> Днепропетровская обл.</div>
+          <div className='text__bold'>г.Каменское</div>
+          <div className='text__bold'>51928</div>
+          <div className='text__grey text__small'>https://github.com/RomanEvgenovichLoboda</div>
+        
+        </div>
+      </div>
+
+      
+       
+    </div>
+  );
+}
+
+export default App;
